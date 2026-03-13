b3951340ec38791e61707b7f4a5fedddff619a70
AlphaAnas <96944434+AlphaAnas@users.noreply.github.com>
2026-03-13 14:23:00 +0500

    add 2 questions

---
+++ reverseVowelsIn_String.py
@@ -0,0 +1,20 @@
+"""
+345. Reverse Vowels of a String
+Easy
+Topics
+premium lock icon
+Companies
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+Leetcode - medium
+"""
+ 
+
+def reverseVowels(s):
+
+
+
+reverseVowels("IceCreAm")    #Output= "AceCreIm"
+reverseVowels("leetcode") #Output: "leotcede"
