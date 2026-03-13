c3298d9ed8ad98fdc995441827dab4b543c12c39
AlphaAnas <muhd.anas70pe@gmail.com>
2026-03-13 14:47:05 +0500

    add another question

--- reverseVowelsIn_String.py
+++ reverseVowelsIn_String.py
@@ -14,7 +14,22 @@ Leetcode - medium
 
 def reverseVowels(s):
 
+    s = list(s)
+    vowels = ['a', 'e', 'i','o', 'u']
+    v = []
+
+    for i in range(len(s)-1, -1, -1):
+        if s[i].lower() in vowels: v.append(s[i])
+
+    count = 0
+    for i in range(len(s)):
+        if s[i].lower() in vowels:
+            s[i] = v[count]
+            count += 1
+    return "".join(s) 
 
 
-reverseVowels("IceCreAm")    #Output= "AceCreIm"
-reverseVowels("leetcode") #Output: "leotcede"
+
+
+print(reverseVowels("IceCreAm"))    #Output= "AceCreIm"
+print(reverseVowels("leetcode")) #Output: "leotcede"
