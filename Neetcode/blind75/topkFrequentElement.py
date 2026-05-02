e92cbbb703f24e19d6e7993fc77ed135cd19f996
M.Anas <M.Anas@tpsonline.com>
2026-05-02 22:23:30 +0500

    Add question Encode and Decode Strings

--- Neetcode/blind75/topkFrequentElement.py
+++ Neetcode/blind75/topkFrequentElement.py
@@ -33,8 +33,29 @@ def topKFrequent(nums, k):
         return final
 
 '''
-                
 
+
+# //////////////////// LINEAR TIME APPROACH ///////////////////////
+def topKFrequent(nums, k):
+
+    d ={}
+    for n in nums:
+        d[n] = d.get(n, 0) + 1
+    counts = [[] for i in range(len(nums) + 1)]
+
+    for n,c in d.items():
+        counts[c].append(n)    
+    f = []
+    for i in range(len(counts) - 1, 0, -1):
+        for n in counts[i]:
+            f.append(n)
+            if len(f) == k:
+                return f
+
+print(topKFrequent([1,1,1,2,2,3],2 )) # output [1,2]
+print(topKFrequent([1,2,2,3,3,3],2 )) # output [2,3]
+print(topKFrequent([7,7], 1)) # output [7]
+        
 
 
 
