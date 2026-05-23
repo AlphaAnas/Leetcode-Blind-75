00bb03d990e54211a85ad56ceb1edd9c3c306419
AlphaAnas <muhd.anas70pe@gmail.com>
2026-05-23 22:22:47 +0500

    Add optimized version of LinkedList and Sliding window

--- Neetcode/blind75/Sliding_Window/bestTimetoBuyStocks.py
+++ Neetcode/blind75/Sliding_Window/bestTimetoBuyStocks.py
@@ -7,12 +7,26 @@
 def maxProfit(prices: list[int]) -> int:
 
         # ///////////////// brute force ///////////////
-        current_profit = 0
-        for i in range(len(prices) - 1):
-                for j in range(i+1, len(prices)):
-                        val = prices[j] - prices[i]
-                        current_profit = max(current_profit, val)
-        return current_profit
+        # current_profit = 0
+        # for i in range(len(prices) - 1):
+        #         for j in range(i+1, len(prices)):
+        #                 val = prices[j] - prices[i]
+        #                 current_profit = max(current_profit, val)
+        # return current_profit
+
+        # ///////////////////// Two pointer approach - O(n) /////////
+        maxP = 0
+        left = 0
+        right = 1
+
+        while right < len(prices):
+                if prices[left] < prices[right]:
+                        profit = prices[right ] - prices[left]
+                        maxP = max(maxP, profit)
+                else:
+                        left = right
+                right += 1
+        return maxP
 
 
 print(maxProfit(prices = [10,1,5,6,7,1])) # 6
